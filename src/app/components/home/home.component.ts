fa863422a785ced38fe4882313735710a4f21ea3
BeataLiszkiewicz <beata.liszkiewicz@gmail.com>
2023-10-09 22:18:55 +0200

    new colors for home page

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, ElementRef, HostListener, ViewChild } from '@angular/core';
 import listOfCities from '../../../assets/data/allCities.json';
+import { BehaviorSubject, Observable, Subscription, interval } from 'rxjs';
 
 @Component({
   selector: 'app-home',
@@ -7,36 +8,46 @@ import listOfCities from '../../../assets/data/allCities.json';
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent {
+  cityList: any;
   cityName: string = 'Antalya';
   cityNumber: number = 1;
-  cityList: any;
+  cityPosition!: Number;
+  interval: number = 100000;
   placePicture: string = 'assets/pictures/Antalya.jpg';
   placePictureAgain: string = 'assets/pictures/Antalya.jpg';
 
+  @ViewChild('destinationBackground', { static: true })
+  destinationBackground!: ElementRef;
+
+  @HostListener('window:scroll', ['$event'])
+  onWindowScroll() {
+    if (
+      this.destinationBackground.nativeElement.getBoundingClientRect().top <
+        300 &&
+      this.destinationBackground.nativeElement.getBoundingClientRect().top >
+        -500
+    ) {this.interval=1000
+    }else{
+      this.interval=100000
+    }
+  }
+
   ngOnInit() {
+    
+    console.log(this.interval);
     this.cityList = listOfCities;
     setInterval(() => {
       this.changeBackgroundCity(this.cityNumber);
-    }, 8000);
+    }, this.interval);
   }
 
-  changeBackgroundCity(param: number) {
-    this.cityName =""
-    this.placePictureAgain = `assets/pictures/${this.cityList[param].picture}.jpg`;
-    
 
-    setTimeout(() => {
-      this.cityName = this.cityList[param].city;
-    },1000);
+  changeBackgroundCity(param: number) {
+    this.placePictureAgain = `assets/pictures/${this.cityList[param].picture}.jpg`;
 
     setTimeout(() => {
       this.placePicture = `assets/pictures/${this.cityList[param].picture}.jpg`;
-    },4000);
-
-    setTimeout(() => {
-      this.cityName = "";
-      console.log('usunięcie nazwy miasta', this.cityList[param].city)
-    }, 7900);
+    }, 4000);
 
     if (this.cityNumber === 10) {
       this.cityNumber = 0;
