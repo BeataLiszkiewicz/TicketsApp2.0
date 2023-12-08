99f424153b8a9b3d81c9b9e49e7e096b3e6ae399
BeataLiszkiewicz <beata.liszkiewicz@gmail.com>
2023-12-08 22:23:53 +0100

    seat choice and cancellation works

--- src/app/components/summary/summary.component.ts
+++ src/app/components/summary/summary.component.ts
@@ -1,34 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, ViewChildren, QueryList } from '@angular/core';
 import { BookPlaneService } from 'src/app/services/book-plane.service';
 import { PassengerService } from 'src/app/services/passenger.service';
 import { Flight } from 'src/app/interfaces/flight';
 import { Router } from '@angular/router';
 import { PlaneService } from 'src/app/services/plane.service';
 import FlyDistance from '../../../assets/data/flyDistance.json';
+import { Subscription, fromEvent } from 'rxjs';
 
 @Component({
   selector: 'app-summary',
   templateUrl: './summary.component.html',
-  styleUrls: ['./summary.component.scss']
+  styleUrls: ['./summary.component.scss'],
 })
 export class SummaryComponent {
-  allDetails!:Flight;
+  allDetails!: Flight;
   arrival!: string;
   distance!: any;
-  extraLuggage!:number;
-  extraLuggagePlus!:number;
+  extraLuggage!: number;
+  extraLuggagePlus!: number;
   flyDistance!: any;
-  planeVisible:boolean=false;
+  openId!: number;
+  planeVisible: boolean = false;
+  seatSubscription!: Subscription;
 
-  constructor(private bookService: BookPlaneService,
-    private passengerService:PassengerService,
-    private planeService:PlaneService,
+  constructor(
+    private bookService: BookPlaneService,
+    private passengerService: PassengerService,
+    private planeService: PlaneService,
     private router: Router
-    ){}
+  ) {}
 
-  ngOnInit(){
+  @ViewChildren('seat') buttons!: QueryList<any>;
+
+  ngOnInit() {
     this.bookService.setBookingButton(true);
-    this.allDetails=this.passengerService.returnDetails();
+    this.allDetails = this.passengerService.returnDetails();
     this.availableLuggageType();
 
     this.arrival = this.passengerService.getOptionDetails('to');
@@ -39,12 +45,11 @@ export class SummaryComponent {
     }
   }
 
-  go(){
-   
-    console.log(this.allDetails)
+  go() {
+    console.log(this.allDetails);
   }
 
-  availableLuggageType(){
+  availableLuggageType() {
     switch (this.allDetails.currency) {
       case 'PLN':
         this.extraLuggage = 150;
@@ -61,12 +66,43 @@ export class SummaryComponent {
     }
   }
 
-  choseSeat(position:number){
-    this.planeVisible=true;
-    console.log(position)
+  choseSeat(position: number) {
+    this.planeVisible = true;
+    this.openId = position;
+
+    this.seatSubscription = fromEvent(
+      document.getElementsByClassName('seat'),
+      'click'
+    ).subscribe({
+      next: (el: any) => {
+        this.allDetails.passengers[position].seat = el.target.innerHTML;
+
+        if (el.target.name === 'clear') {
+          el.target.name = 'checked';
+        } else {
+          this.allDetails.passengers.every((person, index) => {
+            if (person.seat === el.target.innerHTML) {
+              person.seat = '';
+              if (position === index) {
+                el.target.name = 'clear';
+              }
+
+              return false;
+            }
+            return true;
+          });
+        }
+      },
+      error: (err) => console.log('Wystąpił błąd', err),
+    });
   }
 
-  seatClicked(){
-    this.planeVisible=false;
+  seatClicked() {
+    this.planeVisible = false;
+    setTimeout(() => {
+      {
+        this.seatSubscription!.unsubscribe();
+      }
+    }, 10);
   }
 }
